0af4b52bd69203379148a0013cca4d73e8e9e790
Thiago Peres Ataide <thataide@gmail.com>
2022-12-05 16:17:54 -0500

    PoC

---
+++ SourceCode/CreateTrainingFilesWindows/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace CreateTrainingFilesWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(205, 273);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Training Files";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 358);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(645, 233);
+            this.label2.TabIndex = 2;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 428);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Create Training Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SourceCode/CreateTrainingFilesWindows/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace CreateTrainingFilesWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string json = "{\"classifiers\":[";
+
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            if (excelApp != null)
+            {
+                Microsoft.Office.Interop.Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(@"C:\Excel\Data.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                Microsoft.Office.Interop.Excel.Worksheet excelWorksheet = (Microsoft.Office.Interop.Excel.Worksheet)excelWorkbook.Sheets[1];
+
+                Microsoft.Office.Interop.Excel.Range excelRange = excelWorksheet.UsedRange;
+                int rowCount = excelRange.Rows.Count;
+                int colCount = excelRange.Columns.Count;
+
+                List<string> list = new List<string>();
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    Microsoft.Office.Interop.Excel.Range label = (excelWorksheet.Cells[i, 2] as Microsoft.Office.Interop.Excel.Range);
+                    string stringLabel = label.Value.ToString();
+                    if (list.Contains(stringLabel) == false)
+                    {
+                        list.Add(stringLabel);
+                    }
+                }
+                for(int i = 0; i < list.Count; i++)
+                {
+                    json += "{\"name\":\"" + list[i] + "\"},";
+                }
+                //remove the , from last item and close file
+                json = json.Remove(json.Length - 1);
+                json += "],\"documents\":[";
+
+                //i = 2 to start so we skip headers
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    Microsoft.Office.Interop.Excel.Range content = (excelWorksheet.Cells[i, 1] as Microsoft.Office.Interop.Excel.Range);
+                    Microsoft.Office.Interop.Excel.Range label = (excelWorksheet.Cells[i, 2] as Microsoft.Office.Interop.Excel.Range);
+                    string stringContent = content.Value.ToString();
+                    string stringLabel = label.Value.ToString();
+                    //label1.Text = label1.Text + " " + cellValue;
+                    string fileName = "file" + (i - 1).ToString() + ".txt";
+                    createTxtFile(fileName, stringContent);
+                    json += "{\"location\":\"" + fileName + "\",\"language\":\"en-us\",\"classifiers\":[{\"classifierName\":\"" + stringLabel + "\"}]},";
+                }
+                //remove the , from last item and close file
+                json = json.Remove(json.Length - 1) + "]}";
+                createTxtFile("model.json", json);
+
+                label1.Text = "Training files created. Now upload them to Azure Storage.";
+
+                excelWorkbook.Close();
+                excelApp.Quit();
+            }
+        }
+
+        void createTxtFile(string name, string content)
+        {
+            string fileName = @"C:\Excel\Training\" + name;
+
+            try
+            {
+                // Check if file already exists. If yes, delete it.     
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a new file     
+                using (FileStream fs = File.Create(fileName))
+                {
+                    // Add some text to file    
+                    Byte[] fileContent = new UTF8Encoding(true).GetBytes(content);
+                    fs.Write(fileContent, 0, fileContent.Length);
+                }
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+    }
+}
